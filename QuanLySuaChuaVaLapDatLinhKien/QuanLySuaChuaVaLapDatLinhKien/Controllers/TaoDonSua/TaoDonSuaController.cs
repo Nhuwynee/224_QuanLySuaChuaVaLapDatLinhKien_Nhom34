418b010163c86100724f8da119762c694dcef15f
khanhnguvcl <nguyenvukhanh392@gmail.com>
2025-05-07 11:40:57 +0700

    thêm các controller và view từ các UI đã demo

---
+++ QuanLySuaChuaVaLapDatLinhKien/QuanLySuaChuaVaLapDatLinhKien/Controllers/TaoDonSua/TaoDonSuaController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace QuanLySuaChuaVaLapDatLinhKien.Controllers.TaoDonSua
+{
+    public class TaoDonSuaController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ QuanLySuaChuaVaLapDatLinhKien/QuanLySuaChuaVaLapDatLinhKien/Controllers/TaoDonSuaKVL/TaoDonSuaKVLController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace QuanLySuaChuaVaLapDatLinhKien.Controllers.TaoDonSuaKVL
+{
+    public class TaoDonSuaKVLController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
