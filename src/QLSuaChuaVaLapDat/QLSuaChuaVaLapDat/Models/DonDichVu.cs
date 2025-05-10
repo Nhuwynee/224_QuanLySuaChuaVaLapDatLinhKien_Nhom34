8367b0be06046ba29919824cd8aacdc794c3bbbb
Deadvl <shadowboy812004@gmail.com>
2025-05-11 01:55:03 +0700

    chinh ui và nối với sql

---
+++ src/QLSuaChuaVaLapDat/QLSuaChuaVaLapDat/Models/DonDichVu.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace QLSuaChuaVaLapDat.Models;
+
+public partial class DonDichVu
+{
+    [Key]
+    public string IdDonDichVu { get; set; } = null!;
+
+    public string? IdUser { get; set; }
+
+    public string? IdKhachVangLai { get; set; }
+
+    public string IdNhanVienKyThuat { get; set; } = null!;
+
+    public string IdUserTaoDon { get; set; } = null!;
+
+    public string IdLoaiThietBi { get; set; } = null!;
+
+    public string? TenThietBi { get; set; }
+
+    public string LoaiKhachHang { get; set; } = null!;
+
+    public DateTime? NgayTaoDon { get; set; }
+
+    public DateTime? NgayHoanThanh { get; set; }
+
+    public decimal? TongTien { get; set; }
+
+    public string HinhThucDichVu { get; set; } = null!;
+
+    public string LoaiDonDichVu { get; set; } = null!;
+
+    public string? PhuongThucThanhToan { get; set; }
+
+    public string TrangThaiDon { get; set; } = null!;
+
+    public DateTime? NgayChinhSua { get; set; }
+
+}
---
+++ src/QLSuaChuaVaLapDat/QLSuaChuaVaLapDat/Models/QuanLySuaChuaVaLapDatContext.cs
@@ -0,0 +1,22 @@
+﻿﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace QLSuaChuaVaLapDat.Models;
+
+public class QuanLySuaChuaVaLapDatContext : DbContext
+{
+    public QuanLySuaChuaVaLapDatContext(DbContextOptions<QuanLySuaChuaVaLapDatContext> options)
+        : base(options)
+    {
+    }
+
+    public DbSet<User> User { get; set; }
+
+    public DbSet<DonDichVu> DonDichVu { get; set; }
+
+    public DbSet<DonGia> DonGia { get; set; }
+
+    public DbSet<LoaiLoi> LoaiLoi { get; set; }
+
+}
