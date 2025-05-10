7f98ea663a608dc2a7eb293e1473da9a23b36209
Deadvlel <shadowboy812004@.com>
2025-05-11 01:55:03 +0700

    chinh ui và nối với sql

---
+++ src/QLSuaChuaVaLapDat/QLSuaChuaVaLapDat/Models/DonGia.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace QLSuaChuaVaLapDat.Models;
+
+public partial class DonGia
+{
+    [Key]
+    public string IdDonGia { get; set; } = null!;
+
+    public string? IdLoi { get; set; }
+
+    public decimal Gia { get; set; }
+
+    public DateOnly? NgayCapNhat { get; set; }
+
+    public virtual LoaiLoi? IdLoiNavigation { get; set; }
+}
---
+++ src/QLSuaChuaVaLapDat/QLSuaChuaVaLapDat/Models/User.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Data;
+
+namespace QLSuaChuaVaLapDat.Models;
+
+public partial class User
+{
+    [Key]
+    public string IdUser { get; set; } = null!;
+
+    public string? IdRole { get; set; }
+
+    public string? IdPhuong { get; set; }
+
+    public string TenUser { get; set; } = null!;
+
+    public string HoVaTen { get; set; } = null!;
+
+    public string Sdt { get; set; } = null!;
+
+    public string MatKhau { get; set; } = null!;
+
+    public string? DiaChi { get; set; }
+
+    public DateOnly? NgaySinh { get; set; }
+
+    public bool? TrangThai { get; set; }
+
+    public string? Cccd { get; set; }
+
+    public bool? GioiTinh { get; set; }
+
+    public string? ChuyenMon { get; set; }
+
+    public virtual ICollection<DonDichVu> DonDichVuIdNhanVienKyThuatNavigations { get; set; } = new List<DonDichVu>();
+
+    public virtual ICollection<DonDichVu> DonDichVuIdUserNavigations { get; set; } = new List<DonDichVu>();
+
+    public virtual ICollection<DonDichVu> DonDichVuIdUserTaoDonNavigations { get; set; } = new List<DonDichVu>();
+
+}
